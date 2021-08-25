1b3e074ada383d9bea990d39ca58da81d61aa270
RudiBob <rudi.bob.harris@gmail.com>
2021-08-26 01:25:30 +1000

    registration fixes - added additional logging to Info for troubleshooting

--- src/RudiDoes.Provider/Installers/RelatedEntitiesInstaller.cs
+++ src/RudiDoes.Provider/Installers/RelatedEntitiesInstaller.cs
@@ -3,17 +3,35 @@ using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using CluedIn.Core.Installers;
 using CluedIn.RelatedEntities;
+using Microsoft.Extensions.Logging;
 
 namespace CluedIn.RudiDoes.RelatedEntities.Installers
 {
     public class RelatedEntitiesInstaller : IWindsorInstaller
     {
+        private ILogger Log;
+
+        public RelatedEntitiesInstaller(ILogger log)
+        {
+            Log = log;
+        }
+
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
+            Log.LogInformation("[RudiDoes] Begin CluedIn.RudiDoes.RelatedEntities.Installers.Install()");
+
             // RelatedEntitiesUtility.CypherFluentQueriesCount() requires RelatedEntitiesQueries to be registered
             container.Register(Component.For<RelatedEntitiesQueries>().Instance(new RelatedEntitiesQueries()));
 
-            container.Register(CluedInTypes.FromCluedInAssembliesWithServiceFromInterface<IRelatedEntitiesProvider>());
+            // register all the IRelatedEntitiesProvider classes
+            var providers = CluedInTypes.FromCluedInAssembliesWithServiceFromInterface<IRelatedEntitiesProvider>();
+            foreach (var provider in providers)
+            {
+                Log.LogInformation($"[RudiDoes] Registering {provider.GetType()}");
+            }
+            container.Register(providers);
+
+            Log.LogInformation("[RudiDoes] End CluedIn.RudiDoes.RelatedEntities.Installers.Install()");
         }
     }
 }
--- src/RudiDoes.Provider/RudiDoesRelatedEntitiesProvider.cs
+++ src/RudiDoes.Provider/RudiDoesRelatedEntitiesProvider.cs
@@ -3,6 +3,7 @@ using CluedIn.Core;
 using CluedIn.Core.Data;
 using CluedIn.DataStore.Document.Models;
 using CluedIn.RelatedEntities;
+using Microsoft.Extensions.Logging;
 
 namespace CluedIn.Provider.RudiDoes
 {
@@ -10,20 +11,30 @@ namespace CluedIn.Provider.RudiDoes
     {
         public IEnumerable<SuggestedSearch> GetRelatedEntitiesSearches(ExecutionContext context, Entity entity)
         {
+            // use the provided Log object
+            var Log = context.Log;
+
+            Log.LogInformation($"[RudiDoes] RudiDoesRelatedEntitiesProvider.GetRelatedEntitiesSearches({context}, {entity})");
             if (entity.Type != EntityType.Organization)
             {
+                Log.LogInformation("[RudiDoes] Entity is not an Organization - nothing to suggest");
                 return new SuggestedSearch[0];
             }
 
             var searches = new List<SuggestedSearch>();
             if (RelatedEntitiesUtility.CypherFluentQueriesCount("{{RELATIONSHIP}} for {{ENTITY}}", string.Format("{0},{1}", "/RudiDoes", entity.Id.ToString()), context) > 0)
             {
+                Log.LogInformation("[RudiDoes] CypherFluentQueries matches - adding suggested search");
                 searches.Add(new SuggestedSearch {
                 DisplayName = "RudiDoes",
                 SearchQuery = "{{RELATIONSHIP}} for {{ENTITY}}",
                 Tokens = string.Format("{0},{1}", "/RudiDoes", entity.Id.ToString()),
                 Type = "List"
                 });
+            }
+            else
+            {
+                Log.LogInformation("[RudiDoes] CypherFluentQueries does not match - nothing to suggest");
             }
             return searches;
         }
