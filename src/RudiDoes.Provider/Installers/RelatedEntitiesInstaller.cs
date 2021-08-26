4110db28041b108ab423a6065d80b62d5c3a21ec
rudiharris <rha@cluedin.com>
2021-08-26 14:10:22 +1000

    logging improvements and a TODO

--- src/RudiDoes.Provider/Installers/RelatedEntitiesInstaller.cs
+++ src/RudiDoes.Provider/Installers/RelatedEntitiesInstaller.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
@@ -23,13 +24,21 @@ namespace CluedIn.RudiDoes.RelatedEntities.Installers
             // RelatedEntitiesUtility.CypherFluentQueriesCount() requires RelatedEntitiesQueries to be registered
             container.Register(Component.For<RelatedEntitiesQueries>().Instance(new RelatedEntitiesQueries()));
 
+            // TODO: need to figure out exact method for listing all the derived classes without using windsor
+            // {
+            //     // log all the derived classes - that is the intent of our registration below
+            //     // unfortunately castle windsor doesn't offer debug options around a IRegistration object
+            //     var classes = System.AppDomain.CurrentDomain.GetAssemblies()
+            //                .SelectMany(assembly => assembly.GetTypes())
+            //                .Where(type => type.IsSubclassOf(typeof(IRelatedEntitiesProvider)));
+            //     foreach (var derivedClass in classes)
+            //     {
+            //         Log.LogInformation($"[RudiDoes] Registering {derivedClass}");
+            //     }
+            // }
+
             // register all the IRelatedEntitiesProvider classes
-            var providers = CluedInTypes.FromCluedInAssembliesWithServiceFromInterface<IRelatedEntitiesProvider>();
-            foreach (var provider in providers)
-            {
-                Log.LogInformation($"[RudiDoes] Registering {provider.GetType()}");
-            }
-            container.Register(providers);
+            container.Register(CluedInTypes.FromCluedInAssembliesWithServiceFromInterface<IRelatedEntitiesProvider>());
 
             Log.LogInformation("[RudiDoes] End CluedIn.RudiDoes.RelatedEntities.Installers.Install()");
         }
