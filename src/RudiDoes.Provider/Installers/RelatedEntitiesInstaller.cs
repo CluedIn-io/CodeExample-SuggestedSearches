fb7eababbc0175bbe8fbe76e1265fc2c66824213
RudiBob <rudi.bob.harris@gmail.com>
2021-08-27 01:00:05 +1000

    registering IRelatedEntitiesProvider logging improvement - disabled integration tests - added check for expected IRelatedEntitiesProvider classes

--- src/RudiDoes.Provider/Installers/RelatedEntitiesInstaller.cs
+++ src/RudiDoes.Provider/Installers/RelatedEntitiesInstaller.cs
@@ -24,18 +24,19 @@ namespace CluedIn.RudiDoes.RelatedEntities.Installers
             // RelatedEntitiesUtility.CypherFluentQueriesCount() requires RelatedEntitiesQueries to be registered
             container.Register(Component.For<RelatedEntitiesQueries>().Instance(new RelatedEntitiesQueries()));
 
-            // TODO: need to figure out exact method for listing all the derived classes without using windsor
-            // {
-            //     // log all the derived classes - that is the intent of our registration below
-            //     // unfortunately castle windsor doesn't offer debug options around a IRegistration object
-            //     var classes = System.AppDomain.CurrentDomain.GetAssemblies()
-            //                .SelectMany(assembly => assembly.GetTypes())
-            //                .Where(type => type.IsSubclassOf(typeof(IRelatedEntitiesProvider)));
-            //     foreach (var derivedClass in classes)
-            //     {
-            //         Log.LogInformation($"[RudiDoes] Registering {derivedClass}");
-            //     }
-            // }
+            {
+                // log all the classes that implement IRelatedEntitiesProvider
+                // (that is the intent of our registration below
+                // unfortunately castle windsor doesn't offer debug options around a IRegistration object)
+                var classes = System.AppDomain.CurrentDomain.GetAssemblies()
+                           .SelectMany(assembly => assembly.GetTypes())
+                    .Where(type => type.GetInterfaces().Contains(typeof(IRelatedEntitiesProvider)))
+                    .Where(type => type.AssemblyQualifiedName.Contains("RudiDoes"));
+                foreach (var classRegistered in classes)
+                {
+                    Log.LogInformation($"[RudiDoes] Registering {classRegistered}");
+                }
+            }
 
             // register all the IRelatedEntitiesProvider classes
             container.Register(CluedInTypes.FromCluedInAssembliesWithServiceFromInterface<IRelatedEntitiesProvider>());
